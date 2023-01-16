b78350a010ea509be5f1067ea1e39bdabdb245a2
underpig1 <chasepgraham@icloud.com>
2023-01-15 22:42:19 -1000

    enable perms

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,23 @@
+var socket;
 
+function join(p = "Enter join code:") {
+    const code = prompt(p);
+    if (code != null) {
+        try {
+            if (!key.validate(code)) throw "Invalid key";
+            else {
+                socket = io(`http://${key.apply(code, key.decode)}:8000`);
+                if (!socket.connected) throw "No connection";
+            }
+            alert("Connected!");
+        }
+        catch (error) {
+            join("Invalid join code. Make sure you're connected to the same wifi network and try again:");
+        }
+    }
+}
+
+join();
 
 function request_perms() {
     DeviceMotionEvent.requestPermission().then(response => {
