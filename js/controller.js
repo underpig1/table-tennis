c4776ff3ad9ce60ff2b4bc1e7b784fc877dcb75d
underpig1 <chasepgraham@icloud.com>
2023-01-15 22:39:53 -1000

    debug perms

--- js/controller.js
+++ js/controller.js
@@ -1,28 +1,4 @@
-var socket;
 
-const refrate = 1 / 60;
-
-function join(p = "Enter join code:") {
-    const code = prompt(p);
-    if (code != null) {
-        try {
-            if (!key.validate(code)) throw "Invalid key";
-            else {
-                socket = io(`http://${key.apply(code, key.decode)}:8000`);
-                if (!socket.connected) throw "No connection";
-            }
-            alert("Connected!");
-        }
-        catch (error) {
-            alert(error.message);
-            join("Invalid join code. Make sure you're connected to the same wifi network and try again:");
-        }
-    }
-}
-
-window.addEventListener("error", (e) => alert(`${e.type}: ${e.message}`));
-
-join();
 
 function request_perms() {
     DeviceMotionEvent.requestPermission().then(response => {
--- js/public_key.js
+++ js/public_key.js
@@ -1,6 +1,6 @@
 const key = {
     map: "0123456789.",
-    public: "ABCDEFGHJKM",
+    public: "978W56Z3X4120A",
     encode: (k) => key.public[key.map.indexOf(k)],
     decode: (k) => key.map[key.public.indexOf(k)],
     apply: (map, call) => key.correct(map).split("").map((k) => call(k)).join(""),
